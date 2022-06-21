50e586bf2e5063c076b8a7d0e6948fefcaa8195f
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2022-06-21 19:52:21 +0200

    make code more clear

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <vector>
 
+// node can be of type fork or leaf
 enum Type {F, L};
 
-struct NodeSerialized{
-    int value;
-    Type type;
-    int sons[4];
-};
-
+// tree structure
 struct Node{
-    int value;
+    int value; // represents the data held by the node (could be mass, position, etc.)
     Type type;
     struct Node *nw;
     struct Node *ne;
@@ -18,13 +14,15 @@ struct Node{
     struct Node *sw;
 };
 
-NodeSerialized newNodeSerialized(int key, Type t){
-    NodeSerialized ns = {key, t, {-1,-1,-1,-1}};
-    return ns;
-}
+// flat node
+struct NodeSerialized{
+    int value; // represents the data held by the node (could be mass, position, etc.)
+    Type type;
+    int children[4]; // link to the array positions of the 4 possible children of the node
+};
 
-Node* newNode(int key, Type t)
-{
+// creates a new node with no children
+Node* newNode(int key, Type t){
     Node* temp = new Node;
     temp->value = key;
     temp->type = t;
@@ -32,6 +30,13 @@ Node* newNode(int key, Type t)
     return (temp);
 }
 
+// creates a serialized node with no children (the array of children is set to -1)
+NodeSerialized newNodeSerialized(int key, Type t){
+    NodeSerialized ns = {key, t, {-1,-1,-1,-1}};
+    return ns;
+}
+
+// function creating a tree example
 Node* createTree(){
     Node *root           = newNode(0, F);
     root->nw             = newNode(1, F);
@@ -58,82 +63,119 @@ Node* createTree(){
     return root;
 }
 
-Node* createTreeFromVector(Node* root, std::vector<NodeSerialized> values, int id){
-    root->value = values[id].value;
-    root->type = values[id].type;
-    root->nw = root->sw = root->ne = root->se = NULL;
-    if(root->type == F){
-        root->nw = createTreeFromVector(new Node, values, values[id].sons[0]);
-        root->ne = createTreeFromVector(new Node, values, values[id].sons[1]);
-        root->se = createTreeFromVector(new Node, values, values[id].sons[2]);
-        root->sw = createTreeFromVector(new Node, values, values[id].sons[3]);
-    }
-    return root;
-}
-
-void traverse_tree(Node* root){
+// pre-order DFS of the tree to print in console
+void printTraverseTree(Node* root){
     std::cout << "(" << root->value << ", " << (root->type == L ? "L" : "F") << ")"<<std::endl;
+
+    // if the node is a leaf then I can stop because it means there are no more children
     if(root->type == L){
         return;
+
+    //otherwise it's a fork so I need to visit the 4 children
     }else{
-        traverse_tree(root->nw); traverse_tree(root->ne);
-        traverse_tree(root->se); traverse_tree(root->sw);
+        printTraverseTree(root->nw);
+        printTraverseTree(root->ne);
+        printTraverseTree(root->se);
+        printTraverseTree(root->sw);
     }
 
 }
 
-int createvect(Node* root, std::vector<NodeSerialized>& values){
-    NodeSerialized newNode = newNodeSerialized(root->value, root->type);
-    int my_id = values.size();
-    values.push_back(newNode);
-    int son_id = -1;
-    if(root->type != L){
-        son_id = createvect(root->nw, values);
-        values[my_id].sons[0] = son_id;
-        son_id = createvect(root->ne, values);
-        values[my_id].sons[1] = son_id;
-        son_id = createvect(root->se, values);
-        values[my_id].sons[2] = son_id;
-        son_id = createvect(root->sw, values);
-        values[my_id].sons[3] = son_id;
-    }
-    return my_id;
-}
-
-/*void createvect(Node* root, std::vector<NodeSerialized>& values){
-    values.push_back(newNodeSerialized(root->value, root->type));
-    if(root->type == L){
-        return;
-    }else{
-        createvect(root->nw, values); createvect(root->ne, values);
-        createvect(root->se, values); createvect(root->sw, values);
-    }
-}*/
-
-void printvect(std::vector<NodeSerialized> values){
+// serial traversal of the vector to print in console
+void printVect(std::vector<NodeSerialized> values){
     for(int i = 0; i < values.size(); i++){
         std::cout << "id: " << i << " {" << values[i].value << ", " << (values[i].type == L ? "L" : "F") << "} [";
         for(int j = 0; j < 4; j++){
-            std::cout << " " << values[i].sons[j];
+            std::cout << " " << values[i].children[j];
         }
         std::cout << "]" << std::endl;
     }
 }
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    Node* root = createTree();
-    traverse_tree(root);
 
+/**
+ * When called from main set id as 0 to read the tree from the start of the vector.
+ *
+ * @param root a new empy node that will act as the root of the sub-tree analyzed
+ * @param serializedTree the vector containing the serialized representation of the tree
+ * @param id current index of the node in the array
+ * @return tree(root)
+ */
+Node* createTreeFromVector(Node* root, std::vector<NodeSerialized> serializedTree, int id){
+    //create a new node based on the serial one in serializedTree[id]
+    root->value = serializedTree[id].value;
+    root->type = serializedTree[id].type;
+
+    // for now set children to null
+    root->nw = root->sw = root->ne = root->se = NULL;
+
+    // if the node is a fork I make a recursive call on the children (represented in the array of children)
+    if(root->type == F){
+        // notice how I pass as the new vector id of the function the id of the children
+        root->nw = createTreeFromVector(new Node, serializedTree, serializedTree[id].children[0]);
+        root->ne = createTreeFromVector(new Node, serializedTree, serializedTree[id].children[1]);
+        root->se = createTreeFromVector(new Node, serializedTree, serializedTree[id].children[2]);
+        root->sw = createTreeFromVector(new Node, serializedTree, serializedTree[id].children[3]);
+    }
+    return root;
+}
+
+/**
+ * When called, it will build the representation of the tree inside of a vector
+ *
+ * @param root current root of the sub-tree being traversed
+ * @param serializedTree vector that is going to contain the serialized nodes
+ * @return currentId ,it's the id in which the node is going to be saved inside the array (the main output is 0 because it starts at the root)
+ */
+int createvect(Node* root, std::vector<NodeSerialized>& serializedTree){
+    // I create the serialized version of the node
+    NodeSerialized newNode = newNodeSerialized(root->value, root->type);
+
+    // I save the current vector id in which the serialized node is going to be saved
+    int currentId = serializedTree.size();
+    // I push the data inside the vector
+    serializedTree.push_back(newNode);
+
+    // this variable is going to temporarily store the vector ids in which the children will end up
+    int sonId = -1;
+    // if the node is a fork I need to save the children in the vector and obtain their position id inside the vector
+    if(root->type == F){
+        // note that the recursive call is going to return the id position in which the children node is going to be saved
+        sonId = createvect(root->nw, serializedTree);
+        // I save the children id
+        serializedTree[currentId].children[0] = sonId;
+        sonId = createvect(root->ne, serializedTree);
+        serializedTree[currentId].children[1] = sonId;
+        sonId = createvect(root->se, serializedTree);
+        serializedTree[currentId].children[2] = sonId;
+        sonId = createvect(root->sw, serializedTree);
+        serializedTree[currentId].children[3] = sonId;
+    }
+
+    // I return the node id in the vector
+    return currentId;
+}
+
+int main() {
+    std::cout << "FIRST ITERATION" << std::endl;
+    // I create an example tree
+    Node* root = createTree();
+    // I print it in console
+    printTraverseTree(root);
+
+    // I create a vector and save inside of it the structure of the tree
     std::vector<NodeSerialized> values;
     createvect(root, values);
-    printvect(values);
+    // I print the vector in console
+    printVect(values);
 
-    std::cout << "Hello, World!" << std::endl;
+    // reverse process:
+    // I construct the tree from the vector and then print them
+    std::cout << "SECOND ITERATION" << std::endl;
     Node* root2 = createTreeFromVector(new Node, values, 0);
-    traverse_tree(root2);
+    printTraverseTree(root2);
     std::vector<NodeSerialized> values2;
     createvect(root2, values2);
-    printvect(values2);
+    printVect(values2);
     return 0;
 }
